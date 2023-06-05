db8bc1551b3cf0058df1a22bc82349471cf75656
urielch <uriel.chemouni@gmail.com>
2023-06-05 08:59:18 +0300

    order icons by name

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/bi":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-bi@1.0.8/mod.ts",
- *     "react-icons/bi/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-bi@1.0.8/ico/",
+ *     "react-icons/bi":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-bi@1.0.9/mod.ts",
+ *     "react-icons/bi/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-bi@1.0.9/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Import an icon without import_map by and afer loading all icons from the lib bi
  * 
  * ```ts
- * import { BiAbacus } from "https://deno.land/x/react_icons_bi@1.0.8/mod.ts"
+ * import { BiAbacus } from "https://deno.land/x/react_icons_bi@1.0.9/mod.ts"
  * ```
  * 
  * ## import_map import an icon from all icons
