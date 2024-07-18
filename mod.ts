3698e91e1f4908833edf3951f9941d95b1d896b1
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:06 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/bi)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/bi
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/bi
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # BoxIcons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/bi)](https://jsr.io/@preact-icons/bi)
- * 
- * **License** [CC BY 4.0 License](https://github.com/atisawd/boxicons/blob/master/LICENSE)
- * 
- * **Project** [https://github.com/atisawd/boxicons](https://github.com/atisawd/boxicons)
- * 
- * [See available icons here](https://react-icons.deno.dev/bi)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/bi
- * dnpx jsr add @preact-icons/bi
- * pnpm dlx jsr add @preact-icons/bi
- * bunx jsr add @preact-icons/bi
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import BiAbacus from "react-icons/bi/BiAbacus.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
