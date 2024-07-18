3e5b817923af54df13b14c5a14ea6b739ef66dd5
urielch <uriel.chemouni@gmail.com>
2024-07-18 09:54:23 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,39 +10,56 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/bi)
  * 
- * ## import_map.json
+ * ## install packahe
  * 
- * For a transparent usage:
- * 
- * ```json
- * {
- *   "imports": {
- *     "@preact-icons/common": "jsr:@preact-icons/common@^1.0.10",
- *     "preact": "npm:preact@10.22.1",
- *     "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *     "preact/hooks": "npm:preact@10.22.1/hooks",
- *     "react-icons/bi": "jsr:@preact-icons/bi@^1.0.11/mod.ts",
- *     "react-icons/bi/": "jsr:@preact-icons/bi@^1.0.11/ico/",
- *   }
- * }
+ * ```bash
+ * deno add @preact-icons/bi
+ * dnpx jsr add @preact-icons/bi
+ * pnpm dlx jsr add @preact-icons/bi
+ * bunx jsr add @preact-icons/bi
  * ```
  * 
- * ## Import an icon without import_map by and afer loading all icons from the lib bi
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
  * 
- * ```ts
- * import { BiAbacus } from "jsr:preact-icons/bi@1.0.11/mod.ts"
+ * 
+ * # BoxIcons icons for preact
+ * 
+ * [![JSR](https://jsr.io/badges/@preact-icons/bi)](https://jsr.io/@preact-icons/bi)
+ * 
+ * **License** [CC BY 4.0 License](https://github.com/atisawd/boxicons/blob/master/LICENSE)
+ * 
+ * **Project** [https://github.com/atisawd/boxicons](https://github.com/atisawd/boxicons)
+ * 
+ * [See available icons here](https://react-icons.deno.dev/bi)
+ * 
+ * ## install packahe
+ * 
+ * ```bash
+ * deno add @preact-icons/bi
+ * dnpx jsr add @preact-icons/bi
+ * pnpm dlx jsr add @preact-icons/bi
+ * bunx jsr add @preact-icons/bi
  * ```
  * 
- * ## import_map import an icon from all icons
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
+ * 
+ * 
+ * ## import an icon from all icons
  * 
  * ```ts
- * import { BiAbacus } from "react-icons/bi"
+ * import { BiAbacus } from "@preact-icons/bi"
  * ```
  * 
  * ## import a single icon, downloading just one icon
  * 
  * ```ts
- * import { BiAbacus } from "react-icons/bi/BiAbacus.ts"
+ * import { BiAbacus } from "react-icons/bi/BiAbacus"
  * ```
  * 
  * or using default export
